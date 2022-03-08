fc6f10c5c0315387a593d6e693c90678d12652ad
Timur Kramár <timurjan.kramar123@gmail.com>
2022-03-08 17:44:45 +0100

    week_4, sizes of data types

---
+++ src/week_4.c
@@ -0,0 +1,30 @@
+//
+// Created by Timur Kramar on 08/03/2022.
+//
+
+#include <printf.h>
+
+int main(void){
+    char a;
+    short int b = 256;
+    int c;
+    long d;
+    long long e;
+    float f;
+    double g;
+    long double h;
+
+    printf("char int: %lu\n", sizeof(a));
+    printf("short int: %lu\n", sizeof(b));
+    printf("int: %lu\n", sizeof(c));
+    printf("long: %lu\n", sizeof(d));
+    printf("long long: %lu\n", sizeof(e));
+    printf("float: %lu\n", sizeof(f));
+    printf("double: %lu\n", sizeof(g));
+    printf("long double: %lu\n", sizeof(h));
+
+    short int *p = &b;
+
+    a = *p;
+    printf("a = %d", a);
+}
