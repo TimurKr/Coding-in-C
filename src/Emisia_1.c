2f7d4d8582c8496614ef35f163cde65e64a17282
Timur Kramár <timurjan.kramar123@gmail.com>
2022-04-17 20:37:38 +0200

    emisia_1, v2, funguje, ale chyby ak niektoré body ležia na priamkach, treba lepšie hodnotenie či to sú alebo nie su prierezy ak ležia na priamke.

--- src/Emisia_1.c
+++ src/Emisia_1.c
@@ -15,10 +15,13 @@ unsigned int uholnik_roztaty_priamkou_pocet_komponent(
     float diff = y[l-1] - (float) - (a*x[l-1] + c) / (float) b;
     if (diff > 0) {
         last_pos = 1;
+        printf("Poloha -1-veho je nad priamkou\n");
     } else if (diff < 0) {
         last_pos = -1;
+        printf("Poloha -1-veho je pod priamkou\n");
     } else {
         last_pos = 0;
+        printf("Poloha -1-veho je na priamke\n");
     }
 
     for (int i = 0; i < l; i++) {
@@ -26,15 +29,18 @@ unsigned int uholnik_roztaty_priamkou_pocet_komponent(
         diff = y[i] - (float) - (a*x[i] + c) / (float) b;
         if (diff > 0) {
             new_pos = 1;
+            printf("Poloha %d-teho je nad priamkou\n", i);
         } else if (diff < 0) {
             new_pos = -1;
+            printf("Poloha %d-teho je pod priamkou\n", i);
         } else {
             new_pos = 0;
+            printf("Poloha %d-teho je na priamke\n", i);
         }
 
                 // Zisti, či sa zmenila poloha, ak hej, musel byť prienik
-        if (new_pos != last_pos) {
-            //printf("medzi bodom %d a %d je prienik", i-1, i);
+        if (new_pos != 0 && last_pos != 0 && new_pos != last_pos) {
+            printf("Medzi bodom %d a %d je prienik\n", i-1, i);
             prienik_counter++;
         }
         last_pos = new_pos;
@@ -43,14 +49,12 @@ unsigned int uholnik_roztaty_priamkou_pocet_komponent(
 }
 
 int main(void) {
-    float x[5] = {};
-    float y[5] = {};
+    float x[5] = {1,3.4,4.75,4,3};
+    float y[5] = {0.7,1,2.14,3.7,1.7};
     float a, b, c;
-    a = 5;
-    b = 6;
-    c = 2;
-
-    printf("Našli sme %d preienikov, to znamená, že uholnik bol rozdelený na %d častí.",
-           uholnik_roztaty_priamkou_pocet_komponent(x,y,5,a,b,c),
-           uholnik_roztaty_priamkou_pocet_komponent(x,y,5,a,b,c)/2);
+    a = -0.7;
+    b = 1;
+    c = 0;
+    unsigned int n = uholnik_roztaty_priamkou_pocet_komponent(x,y,5,a,b,c);
+    printf("\n\nNašli sme %d prienikov, to znamená, že uholnik bol rozdelený na %d častí.\n\n", n, n/2+1);
 }
