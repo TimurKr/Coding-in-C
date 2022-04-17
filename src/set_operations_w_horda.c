e7c00702a326c88ea29f1d29380b4bdf12f95c31
Timur Kramár <timurjan.kramar123@gmail.com>
2022-04-17 22:47:31 +0200

    Working Union

--- src/set_operations_w_horda.c
+++ src/set_operations_w_horda.c
@@ -36,9 +36,9 @@ void heapify_node(int *list, unsigned int list_length, unsigned int root_index){
             // Ak nejaké dieťa je väčšie, vymeň ho s rodičom
             // a pokračuj v heapify o level hlbšie
     if (largest_index != root_index) {
-        pis_mnozinu(list, list_length);
+//        pis_mnozinu(list, list_length);
         swap(largest_index, root_index, list);
-        pis_mnozinu(list, list_length);
+//        pis_mnozinu(list, list_length);
         heapify_node(list, list_length, largest_index);
     }
 }
@@ -55,13 +55,36 @@ void heap_sort(int *list, unsigned int list_length){
     for (unsigned int i = list_length-1; i > 1; i--){
         heapify_node(list, i,0);
         swap(0, i-1, list);
-        pis_mnozinu(list, list_length);
+//        pis_mnozinu(list, list_length);
     }
-    pis_mnozinu(list, list_length);
+//    pis_mnozinu(list, list_length);
+}
+
+void Union(int *a, int *b, int *c, unsigned int a_l, unsigned int b_l){
+    unsigned int c_l = a_l + b_l;
+    for (unsigned int i = 0; i < a_l; i++){
+        c[i] = a[i];
+    }
+    for (unsigned int i = a_l; i < c_l; i++){
+        c[i] = b[i-a_l];
+    }
+    pis_mnozinu(c,c_l);
+}
+
+void Intersection(int *a, int *b, int *d, unsigned int a_l, unsigned int b_l){
+    heap_sort(a,a_l);
+    heap_sort(b,b_l);
 }
 
 int main(void){
-    int a[n] = {4, 2, 8, 4, 14, 12, 53, 13, 11, 9, 1, 10, 20};
-
-    heap_sort(a,n);
+    int a[5] = {4, 2, 8, 4, 14};
+    int b[7] = {6, 2, 16, 3, 9, 11, 7};
+    int c[5+7];
+    int d[7];
+    pis_mnozinu(a,5);
+    pis_mnozinu(b,7);
+    pis_mnozinu(c,5+7);
+    Union(a,b,c,5,7);
+    pis_mnozinu(c,5+7);
+    Intersection(a,b,d,5,7)
 }
